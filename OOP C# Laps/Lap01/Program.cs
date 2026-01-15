28283a750316babf5f8c13b4380dceeb2d5beb1c
Yousef721 <yousarsefabedalla255@gmail.com>
2026-01-15 10:56:50 +0200

    Upload

---
+++ OOP C# Laps/Lap01/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Lap01
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter birth year:");
+            int year = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter birth month:");
+            int month = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter birth day:");
+            int day = int.Parse(Console.ReadLine());
+
+            DateTime today = DateTime.Today;
+
+            int totalDays =
+                (today.Year - year) * 365 +
+                (today.Month - month) * 30 +
+                (today.Day - day);
+
+            int years = totalDays / 365;
+            totalDays %= 365;
+
+            int months = totalDays / 30;
+            int days = totalDays % 30;
+
+            Console.WriteLine($"Your age is {years} years, {months} months, {days} days");
+        }
+    }
+}
