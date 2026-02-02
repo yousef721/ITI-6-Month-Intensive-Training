40587e3335abb1b29ed8b0f548c747c26c202215
Yousef721 <yousarsefabedalla255@gmail.com>
2026-02-03 00:27:28 +0200

    Update

--- OOP C# Laps/Lap03/obj/Debug/net9.0/Lap03.AssemblyInfo.cs
+++ OOP C# Laps/Lap03/obj/Debug/net9.0/Lap03.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lap03")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+180bf3fc935867d09d687fc762f6a1591e6359ad")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92039c08e6851fece0a493f37ecdd70fa567e090")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lap03")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lap03")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
