17fddb8a94e76c632d991d7718ae9d54341e1b04
Yousef721 <yousarsefabedalla255@gmail.com>
2026-02-21 19:20:37 +0200

    Upload Source Control Tasks

--- OOP C# Laps/Lap06/obj/Debug/net9.0/Lap06.AssemblyInfo.cs
+++ OOP C# Laps/Lap06/obj/Debug/net9.0/Lap06.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lap06")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d404cd2aa38c669a21a154a89f5dcddf47f9ca4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+50ccc6b1ceebde983bfc311650e41ce0dffd8f31")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lap06")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lap06")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
