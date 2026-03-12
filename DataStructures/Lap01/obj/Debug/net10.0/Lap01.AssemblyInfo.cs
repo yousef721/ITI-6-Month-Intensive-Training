8d557d8480c0dcf5f8b1efd592da7bb6ec19a8e2
Yousef721 <yousarsefabedalla255@gmail.com>
2026-03-12 18:45:38 +0200

    Update

--- DataStructures/Lap01/obj/Debug/net10.0/Lap01.AssemblyInfo.cs
+++ DataStructures/Lap01/obj/Debug/net10.0/Lap01.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lap01")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f79c0bfc18b7cf874ed40196bc887f4ac2e002e0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+630133ed7a72a54f01d8374e194f6af73af8c2de")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lap01")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lap01")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
