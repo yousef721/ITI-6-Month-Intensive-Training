873ca2b17494538b27819bd75f061c171bbe05da
Yousef721 <yousarsefabedalla255@gmail.com>
2026-04-10 14:24:28 +0200

    Update

--- Advanced C#/Lap02/obj/Debug/net10.0/Lap02.AssemblyInfo.cs
+++ Advanced C#/Lap02/obj/Debug/net10.0/Lap02.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lap02")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+78901cc6891b2efcea6178e29796d64e5fc3383e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d116c224cf95589604d37b3dc45967d64aa0d7d6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lap02")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lap02")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
