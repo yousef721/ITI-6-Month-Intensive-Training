fd61ecc4f92aaa5713c9d9ff827c6b8cb8cf7f8f
Yousef721 <yousarsefabedalla255@gmail.com>
2026-05-18 03:26:18 +0300

    Upload Bonus Lap01 Use Api

---
+++ WebApi2/WebAPIUse/WebAPIUse/Course.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebAPIUse;
+
+public class Course
+{
+    public int id { get; set; }
+    public string name { get; set; }
+    public string desc { get; set; }
+    public int duration { get; set; }
+}
---
+++ WebApi2/WebAPIUse/WebAPIUse/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Net.Http.Json;
+
+namespace WebAPIUse;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        HttpClient client = new HttpClient();
+
+        client.BaseAddress = new Uri("http://localhost:5114/api/");
+
+        // read
+        HttpResponseMessage readResponse = await client.GetAsync("courses");
+
+        if (readResponse.IsSuccessStatusCode)
+        {
+            List<Course>? courses =
+                await readResponse.Content.ReadFromJsonAsync<List<Course>>();
+
+            if (courses != null)
+            {
+                foreach (var course in courses)
+                {
+                    Console.WriteLine($"{course.id} - {course.name}");
+                }
+            }
+        }
+
+        // Create
+        var postCourse = new Course(){name = "Angular", desc = "Description Angular", duration = 0};
+        HttpResponseMessage createResponse = await client.PostAsJsonAsync("courses", postCourse);
+        if (createResponse.IsSuccessStatusCode)
+        {
+            Uri uri = createResponse.Headers.Location!;
+        }
+
+        // Update
+        postCourse.duration = 50;
+        HttpResponseMessage updateResponse = await client.PutAsJsonAsync("courses", postCourse);
+
+        // delete
+        HttpResponseMessage deleteResponse = await client.DeleteAsync("courses/1003");
+    }
+}
